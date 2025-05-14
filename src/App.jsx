f833932713d455ec9024838ea15a0d61b8be1073
Tejas-Barde <tjbarde30@gmail.com>
2025-05-14 16:34:28 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+/* eslint-disable no-empty */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'; 
+import authService from './appwrite/auth';
+import {login,logout} from './store/AuthSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-
-  return (
-    <>
-      <h1>Mega Blogs</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(false);
+  const dispatch = useDispatch();
+  
+  // useEffect(()=>{
+  //   authService.getCurrentUser()
+  //     .then((userData)=>{
+  //       if(userData){
+  //         dispatch(login({userData}));
+  //       }
+  //       else{
+  //         dispatch(logout())
+  //       }
+  //     })
+  //     .finally(()=>{setLoading(false);})
+  // },[])
+  return !loading ? (
+    <div className='min-h-screen'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+          
+          </main>
+          <Footer/>
+        </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from 'conf.js';
+import conf from '../conf/conf.js';
 import { Client,Account, ID } from 'appwrite';
 
 export class AuthService{
@@ -53,3 +53,5 @@ export class AuthService{
         }
     }
 }
+const authService = new AuthService();
+export default authService;
